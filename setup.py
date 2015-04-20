cb7b07e509d63182ddbbd7868782b08b2fb2c034
Erik de Jonge <erik@a8.nl>
2015-04-20 08:30:18 +0200

    pip Monday 20 April 2015 (week:16 day:110), 08:30:16

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ standard_library.install_aliases()
 
 from setuptools import setup
 setup(name='k8svag',
-      version='23',
+      version='24',
       description='Kubernetes Vagrant Provisioning and management script',
       url='https://github.com/erikdejonge/k8svag',
       author='Erik de Jonge',
